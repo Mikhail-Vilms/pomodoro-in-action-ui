112cae9377f17a661632a6dabfa58cad75658c17
Mikhail-Vilms <mikhail.vilms@gmail.com>
2020-02-27 19:58:56 -0600

    FEAT: "welcome page" has been added

--- pomodoro-in-action-ui/src/app/app.module.ts
+++ pomodoro-in-action-ui/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeToolbarComponent } from './welcome-toolbar/welcome-toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    WelcomeToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    // Angular material components
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
